40e4e43aa30b95a5cfb0103bdf65fa436ca03bfc
suusan42 <suusan42@gmail.com>
2022-02-06 13:03:38 +0900

    新規登録

---
+++ index.js
@@ -0,0 +1,7 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    let win = new BrowserWindow({ width:800,height:400,webPreferences:{ nodeIntegration:true} });
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
