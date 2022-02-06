6810037a0e6c7c117228a4289023c6fb0e0f7d77
suusan42 <suusan42@gmail.com>
2022-02-06 17:26:40 +0900

    インデントのみ

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-    let win = new BrowserWindow({ width:800,height:400,webPreferences:{ nodeIntegration:true} });
+    let win = new BrowserWindow(
+            { 
+                width:800,
+                height:400,
+                webPreferences:{nodeIntegration:true} 
+            }
+        );
     win.loadFile('index.html');
 }
 app.whenReady().then(createWindow);
