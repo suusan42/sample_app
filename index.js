12c470811ecf98bcff522ab52337cf91c4df1ec5
suusan42 <suusan42@gmail.com>
2022-02-13 04:49:08 +0900

    ２月１２日

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 
+const html =    '<html>'
+            +   '<head><title>HTML STRING</title></head>'
+            +   '<body><h1>HTML STRING</h1><p>This is string content.</p><body>'
+            +   '</html>';
+
 function createWindow() {
     let win = new BrowserWindow(
             { 
@@ -8,6 +13,6 @@ function createWindow() {
                 webPreferences:{nodeIntegration:true} 
             }
         );
-    win.loadFile('index.html');
-}
+        win.loadURL('data:text/html;charset=utf-8,' + html);
+    }
 app.whenReady().then(createWindow);
